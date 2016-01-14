0eed2e1eecfee2641fd700cfc404015d96b8c0c0
sfmDev <sfmdeveloper@icloud.com>
2016-01-14 10:24:03 +0800

    Initial Commit

---
+++ AVFoundationCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFoundationCamera
+//
+//  Created by 石峰铭 on 16/1/14.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVFoundationCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundationCamera
+//
+//  Created by 石峰铭 on 16/1/14.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
